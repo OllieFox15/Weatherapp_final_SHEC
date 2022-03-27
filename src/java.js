0e71e6264dfde105695162c5cf7226e07df9b224
OllieFox15 <olliefox15@protonmail.com>
2022-03-28 10:25:27 +1300

    time updated

--- src/java.js
+++ src/java.js
@@ -11,14 +11,30 @@ function handleSubmit(event) {
 }
 search("Moscow");
 
-/*function formatDate(timestamp) {
+function formatDate(timestamp) {
   //calculate the date
   let date = new Date(timestamp);
-  let hours = date.gethours();
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = date.getMinutes();
-  let day = date.getDay();
-  return `${day} ${hours}: ${minutes}`;
-}*/
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -26,15 +42,23 @@ function displayTemperature(response) {
 
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  //let dateTimeElement = document.querySelector("#dateTime");
-
+  let dateTimeElement = document.querySelector("#dateTime");
+  let iconElement = document.querySelector("#icon");
   let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = response.data.wind.speed;
-  //dateTimeElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}.png`
+  );
+  dateTimeElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let form = document.querySelector("#searchform");
